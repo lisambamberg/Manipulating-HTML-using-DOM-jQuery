16c91f657dd0fa8c2a800a81593f87e9d8ade2d2
lisambamberg <lisambamberg@gmail.com>
2020-09-23 14:08:46 -0500

    Reworking objective 1 and 2

--- jQuery/app.js
+++ jQuery/app.js
@@ -1,8 +1,18 @@
 $(function () {
     //Objective 1. 
     let btn = $("<button></button>");
-    $("body").append(btn);
+    $(btn).css("height", "30px").css("width","30px");
     $("btn").click(function () {
         alert("Nice message!");
     })
+    $("body").append(btn);
+
+    //Objective 2. 
+    $("form").submit(function () {
+        let textValue = $("input[name=inputInfo]").val();
+        alert(textValue);
+    });
 })
+
+
+
