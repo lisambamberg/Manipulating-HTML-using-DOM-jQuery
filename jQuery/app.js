476ebdc92970c328f8bce0b8256f2e735fb21161
lisambamberg <lisambamberg@gmail.com>
2020-09-23 13:27:18 -0500

    Created button with alert using jQuery

---
+++ jQuery/app.js
@@ -0,0 +1,8 @@
+$(function () {
+    //Objective 1. 
+    let btn = $("<button></button>");
+    $("body").append(btn);
+    $("btn").click(function () {
+        alert("Nice message!");
+    })
+})
